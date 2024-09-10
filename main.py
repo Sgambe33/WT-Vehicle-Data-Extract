0a20be4cadc7d81948dc962c2a43089e7e7e387f
Sgambe33 <80386324+Sgambe33@users.noreply.github.com>
2024-09-10 19:06:16 +0200

    Removed update check
    Removed since everyone can run the script whenever they want

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 from multiprocessing import Process
 from os import path
-import time
 from db.inserter import update_db
 from utils import *
 from utils.constants import *
 from utils.update_localization import generate_locales
-from utils.updater import update_dataset, update_images, updates_available
+from utils.updater import update_dataset, update_images
 from dotenv import load_dotenv
 
 load_dotenv("/utils/.env")
@@ -21,7 +20,7 @@ def get_vehicle_by_country(country, fetch_uri, file_in_path, vehicle_type="VEHIC
 
     final_vehicles = []
     for index, vehicle in enumerate(all_vehicles):
-        if verbose: cLogger.info(f'{index:2} -> {country.upper()}  {vehicle_type.upper()}  {vehicle}') 
+        if verbose: cLogger.info(f'{index:2} -> {country.upper()}  {vehicle_type.upper()}  {vehicle}')
         try:
             vehicle = create_vehicle(vehicle, fetch_uri)
             if vehicle is None:
@@ -55,14 +54,11 @@ def main(verbose: bool = False, use_multiprocessing: bool = True):
         for nation in COUNTRIES:
             process_country(nation, verbose)
 
+
 if __name__ == '__main__':
-    # if not updates_available():
-    #     cLogger.info("No updates available")
-    #     exit(0)
-    # else:
-        cLogger.info("Updates available")
-        update_dataset()
-        main(verbose=True, use_multiprocessing=True)
-        #update_db()
-        #update_images()
-        #generate_locales("./locales")
+    update_dataset()
+    # TODO: Fix localization for weaponry when using multiprocessing
+    main(verbose=True, use_multiprocessing=False)
+    update_db()
+    update_images()
+    generate_locales("./locales")
